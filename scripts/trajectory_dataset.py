64d51c89a8fc0c7e6adcc14b88f85338dc1b54d8
[ Ramon Correa - Sch Compt Rlcorrea Augmented Intellig - Graduate Student - rlcorrea ] <rlcorrea@c014.sol.rc.asu.edu>
2023-11-04 11:23:45 -0700

    work in progress batching dataset

--- scripts/trajectory_dataset.py
+++ scripts/trajectory_dataset.py
@@ -2,85 +2,154 @@ from __future__ import annotations
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 from glob import glob 
-from vima.trajectory.trajectory_util import load_trajectory_info,obs_load,action_load,trajectory_load
+from vima.trajectory.trajectory_util import load_trajectory_info,obs_load,action_load,trajectory_load,prepare_prompt
 from torch.utils.data import Dataset 
 import torch 
 import numpy as np 
 import pdb 
 import pickle as pkl 
 from torch.utils.data import ConcatDataset,DataLoader 
-class traj_object():
-    def __init__(self,traj_path,device='cpu'): 
-        self.traj_path = traj_path 
-        self.traj_id = self.traj_path.split('/')[-1]
-        self.traj_loading = load_trajectory_info
-        self.actions_is_loaded = False 
-        self.obs_is_loaded = False 
-        self.device = device 
-    def get_traj_meta(self): 
-        path = self.traj_path 
-        meta_path = os.path.join(path,'trajectory') 
-        with open(meta_path,'rb') as f:
-            meta = pkl.load(f)
-        self.traj_meta = meta
-    def get_prompt(self):
-        return self._prompt
-    def get_prompt_assets(self): 
-        return self._prompt_assets
-    def get_actions(self):
-        if self.actions_is_loaded: 
-            return self._actions
+
+def index_observation(obs_d,index): 
+    """ observations have the structure (number_of_steps,h,w,c) 
+        we need to return new observations dictionary that is just  (1,h,w,c) 
+        index: which time step we will pull from 
+    """
+    new_dict = dict() 
+    for modality in ['rgb','segm']: 
+        new_dict[modality]= dict()
+        for orientation in ['top','front']: 
+            new_dict[modality][orientation] = obs_d[modality][orientation][index] 
+    new_dict['ee'] = np.asarray(obs_d['ee'][index])
+    return new_dict 
+def index_action(action_d,index,device=None): 
+    """ Same as obvervations but acting overt the actions dictionary 
+
+    """
+    new_dict = dict()
+    ##TODO:#Do a similar approach of indixing above where instead of having (num_steps,x,y) we have (x,y) 
+    for e in action_d.keys(): 
+        if  'position' in e: 
+            new_dict[e] = torch.tensor(action_d[e][index,0:2],device=device)
         else: 
-            self._actions = action_load(self.traj_path) 
-            self.actions_is_loaded= True 
-        return self._actions
-    def get_obs(self):
-        if self.obs_is_loaded: 
-            return self._obs 
-        else:  
-            self._obs = obs_load(self.traj_path)
-            self.obs_is_loaded= True 
-        return self._obs 
-    def get_device(self):
-        return self._device
-    def __len__(self): 
-        return self._traj_meta['steps'] 
-    def __getitem__(self,idx): 
-        c_action,old_actions =  self.index_action(idx,device=self.device)
-        c_obs,old_obs = self.index_observation(idx) 
-        return c_obs,c_action,old_obs,old_actions,idx
-    def index_observation(self,index): 
-        """ observations have the structure (number_of_steps,h,w,c) 
-            we need to return new observations dictionary that is just  (1,h,w,c) 
-            index: which time step we will pull from 
+            new_dict[e] = torch.tensor(action_d[e][index],device=device)
+    return new_dict
+
+
+import random 
+from multiprocessing import Process, Queue,Event 
+def data_proc_worker(task_queue,output_queue,quit_worker_event):
+    """ this is the function of our worker processess. It will load one sample for our batch. 
+    in our use case it will load  the contents of the trajectories and return them one step at a time 
+    task_queue: will have the task ids we need to obtain 
+    output_queue: is where we will write our data to 
+    quit_worker_event: some function to deal with things going sour 
+    """
+    while True: 
+        task = task_queue.get()
+        if task is None:  #needed in case queue becomes empty 
+            break 
+        traj_id,traj_path = task
+        traj_dict = load_trajectory_info(traj_path)
+        traj_meta = traj_dict['meta']
+        num_steps = traj_meta['steps']
+        prompt =  traj_dict['prompt'] 
+        prompt_assets = traj_dict['prompt_assets'] 
+        actions = traj_dict['action']
+        obs = traj_dict['obs']
+        #lets do some helpful preprocessing here 
+        #prompt_token_type, word_batch,image_batch 
+        prommpt_info = prepare_prompt(
+            prompt=prompt,prompt_assets=prompt_assets,views = ['front','top']
+        ) 
+        for i in range(num_steps): 
+            if quit_worker_event.is_set(): 
+                break  
+            c_obs = index_observation(obs,i)
+            c_act = index_action(actions,i) 
+            output_queue.put((traj_id,c_obs,c_act))
+        if quit_worker_event.is_set(): 
+            break 
+    output_queue.put(None) #this is the signal we are done 
+
+
+
+
+
+
+
+class TrajectoryLoader: 
+    def __init__(self,traj_folder,traj_name,n_workers=2,batch_size=8,n_epochs=1,max_queue_size=16):
+        self.trajectory_folder = traj_folder
+        self.traj_name = traj_name 
+        self.n_workers = n_workers 
+        self.batch_size = batch_size 
+        self.n_epochs = n_epochs 
+        self.max_queue_size = max_queue_size 
+        #do some setting 
+        self.set_trajectory_map()
+        self.trajectory_ids = list(self.folder_map.keys())
+        # we need to make the dataset repeatble so we will actually repeat the trajectoryids so we can sample from them 
+        self.trajectories_2_sample = list()   #NOTE: this is my equivalent to demonstration tuples 
+        for i in range(self.n_epochs):
+            c_traj = self.trajectory_ids.copy()
+            random.shuffle(c_traj)
+            self.trajectories_2_sample += c_traj 
+        #we now specify the task queue to be consumed 
+        self.task_queue = Queue() 
+        self.n_steps_processed = 0 #TODO is this samples or epochs 
+        for i,traj_id in enumerate(self.trajectories_2_sample):
+            queue_input = (traj_id,self.trajectories_2_sample[traj_id])
+            self.task_queue.put(queue_input) #this makes it a single element tuple 
+        for _ in range(n_workers):
+            self.task_queue.put(None)
+        self.output_queues = [Queue(maxsize=self.max_queue_size) for _ in range(n_workers)]
+        self.quit_workers_event = Event()
+        self.processes = list() 
+        for output_queue in self.output_queues: 
+            my_proc = Process(traget=data_proc_worker,
+                              args=(self.task_queue,output_queue,self.quit_workers_event),
+                              daemon=True)
+            self.processes.append(my_proc)
+        for procs in self.processes: 
+            procs.start()
+    def __iter__(self):
+        return self 
+
+    def __next__(self): 
+        """ This samples the NEXT BATCH TO USE 
         """
-        obs_d = self.get_obs()
-        c_dict = dict() 
-        old_dict  = dict()
-        for modality in ['rgb','segm']: 
-            c_dict[modality]= dict() 
-            old_dict[modality]= dict() 
-            for orientation in ['top','front']: 
-                old_dict[modality][orientation] = obs_d[modality][orientation][0:(index)]
-                c_dict[modality][orientation] = obs_d[modality][orientation][index]
-        old_dict['ee'] = np.asarray(obs_d['ee'][0:(index)])
-        c_dict['ee'] = np.asarray(obs_d['ee'][index])
-        return c_dict,old_dict
-    def index_action(self,index,device=None): 
-        """ Same as obvervations but acting overt the actions dictionary 
-        """
-        action_d = self.get_actions()
-        new_dict = dict()
-        old_dict = dict()
-        for e in action_d.keys(): 
-            if  'position' in e: 
-                new_dict[e] = torch.tensor(action_d[e][index,0:2],device=device)
-                old_dict[e] = torch.tensor(action_d[e][0:(index),0:2],device=device)
-            else: 
-                new_dict[e] = torch.tensor(action_d[e][index],device=device)
-                old_dict[e] = torch.tensor(action_d[e][0:(index)],device=device)
-        return new_dict,old_dict
+        observations = [] 
+        actions = [] 
+        batch_episode_id = []
+        for i in range(self.batch_size):
+            workitem = self.output_queues[self.n_steps_processed%self.n_workers].get(timeout=10)
+            if workitem is None: 
+                raise StopIteration() 
+            traj_id , observation, action = workitem
+            observations.append(observation)
+            actions.append(action) 
+            batch_episode_id.append(traj_id) 
+            self.n_steps_processed +=1
+        return batch_episode_id, observations, actions
+    def __del__(self):
+        for proc in self.processes:
+            proc.terminate() 
+            proc.join()
+
+
+
+    
             
+
+        
+    def set_trajectory_map(self): 
+        folder = self.trajectory_folder 
+        traj_folders  = glob(folder)
+        folder_map = dict() 
+        folder_map = {e.split('/')[-1]:e for e in traj_folders if not e.endswith('.pkl')}
+        self.folder_map = folder_map 
+
 
 if __name__ =="__main__":
     trajectories = glob("/scratch/rlcorrea/vima_v6/rearrange_then_restore/*") 
@@ -94,3 +163,5 @@ if __name__ =="__main__":
         print('hi')
     
 
+#useful stuff for future 
+meta_file  = os.path.join(folder,'metadata.pkl')
--- vima/trajectory/trajectory_util.py
+++ vima/trajectory/trajectory_util.py
@@ -1,15 +1,27 @@
+from __future__ import annotations
 import pickle as pkl 
 import os 
 from PIL import Image 
 from vima.utils import * 
 from einops import rearrange 
 import numpy as np 
-def load_trajectory_info(traj_path):
-    """ Trajectories are stored in a hierachical fashion 
-    traj_directory/obs.pkl  <--  These are the rgb images representing the scene 
-    traj_directory/action.pkl  <--  These are the actions taken by the oracle 
-    traj_directory/trajectory.pkl  <-- contains number of steps, prmpt, prompt assets etc. useful stuff
-    """
+import os
+import numpy as np
+from einops import rearrange
+import cv2
+from vima.utils import *
+from vima_bench import *
+
+def action_load(traj_path): 
+    with open(os.path.join(traj_path, "action.pkl"), "rb") as f:
+        action = pkl.load(f)
+    return action 
+def trajectory_load(traj_path):
+    with open(os.path.join(traj_path, "trajectory.pkl"), "rb") as f:
+        traj_meta = pkl.load(f)
+    return traj_meta
+
+def obs_load(traj_path): 
     #load  observations 
     with open(os.path.join(traj_path, "obs.pkl"), "rb") as f:
         obs = pkl.load(f)
@@ -34,13 +46,151 @@ def load_trajectory_info(traj_path):
     rgb_dict = {k: np.stack(v, axis=0) for k, v in rgb_dict.items()}
     # add the rgb  representation to the observation dict 
     obs['rgb'] = rgb_dict
+    return obs 
+
+def load_trajectory_info(traj_path):
+    """ Trajectories are stored in a hierachical fashion 
+    traj_directory/obs.pkl  <--  These are the rgb images representing the scene 
+    traj_directory/action.pkl  <--  These are the actions taken by the oracle 
+    traj_directory/trajectory.pkl  <-- contains number of steps, prmpt, prompt assets etc. useful stuff
+    """
     #load actions to take 
-    with open(os.path.join(traj_path, "action.pkl"), "rb") as f:
-        action = pkl.load(f)
-    with open(os.path.join(traj_path, "trajectory.pkl"), "rb") as f:
-        traj_meta = pkl.load(f)
+    obs = obs_load(traj_path)
+    action = action_load(traj_path)
+    traj_meta = trajectory_load(traj_path)
+
     #load the prompt the model would see 
     prompt = traj_meta.pop("prompt")
     #these are assets needed for "rendering" by the model 
     prompt_assets = traj_meta.pop("prompt_assets")
     return  {'prompt':prompt,'prompt_assets':prompt_assets,'traj_meta':traj_meta,'action':action,'obs':obs}
+
+def prepare_prompt(*, prompt: str, prompt_assets: dict, views: list[str]):
+    views = sorted(views)
+    encoding = tokenizer.encode(prompt, add_special_tokens=True)
+    prompt_ids, prompt_tokens = encoding.ids, encoding.tokens
+    assert set(prompt_assets.keys()) == set(
+        [token[1:-1] for token in prompt_tokens if token in PLACEHOLDERS]
+    )
+    filled_prompt = []
+    for id, token in zip(prompt_ids, prompt_tokens):
+        if token not in PLACEHOLDERS:
+            assert "{" not in token and "}" not in token
+            filled_prompt.append(id)
+        else:
+            assert token.startswith("{") and token.endswith("}")
+            asset_name = token[1:-1]
+            assert asset_name in prompt_assets, f"missing prompt asset {asset_name}"
+            asset = prompt_assets[asset_name]
+            obj_info = asset["segm"]["obj_info"]
+            placeholder_type = asset["placeholder_type"]
+            if placeholder_type == "object":
+                objects = [obj_info["obj_id"]]
+            elif placeholder_type == "scene":
+                objects = [each_info["obj_id"] for each_info in obj_info]
+            obj_repr = {
+                "cropped_img": {view: [] for view in views},
+                "bbox": {view: [] for view in views},
+            }
+            for view in views:
+                rgb_this_view = asset["rgb"][view]
+                segm_this_view = asset["segm"][view]
+                bboxes = []
+                cropped_imgs = []
+                for obj_id in objects:
+                    ys, xs = np.nonzero(segm_this_view == obj_id)
+                    if len(xs) < 2 or len(ys) < 2:
+                        continue
+                    xmin, xmax = np.min(xs), np.max(xs)
+                    ymin, ymax = np.min(ys), np.max(ys)
+                    x_center, y_center = (xmin + xmax) / 2, (ymin + ymax) / 2
+                    h, w = ymax - ymin, xmax - xmin
+                    bboxes.append([int(x_center), int(y_center), int(h), int(w)])
+                    cropped_img = rgb_this_view[:, ymin : ymax + 1, xmin : xmax + 1]
+                    if cropped_img.shape[1] != cropped_img.shape[2]:
+                        diff = abs(cropped_img.shape[1] - cropped_img.shape[2])
+                        pad_before, pad_after = int(diff / 2), diff - int(diff / 2)
+                        if cropped_img.shape[1] > cropped_img.shape[2]:
+                            pad_width = ((0, 0), (0, 0), (pad_before, pad_after))
+                        else:
+                            pad_width = ((0, 0), (pad_before, pad_after), (0, 0))
+                        cropped_img = np.pad(
+                            cropped_img,
+                            pad_width,
+                            mode="constant",
+                            constant_values=0,
+                        )
+                        assert cropped_img.shape[1] == cropped_img.shape[2], "INTERNAL"
+                    cropped_img = rearrange(cropped_img, "c h w -> h w c")
+                    cropped_img = np.asarray(cropped_img)
+                    cropped_img = cv2.resize(
+                        cropped_img,
+                        (32, 32),
+                        interpolation=cv2.INTER_AREA,
+                    )
+                    cropped_img = rearrange(cropped_img, "h w c -> c h w")
+                    cropped_imgs.append(cropped_img)
+                bboxes = np.asarray(bboxes)
+                cropped_imgs = np.asarray(cropped_imgs)
+                obj_repr["bbox"][view] = bboxes
+                obj_repr["cropped_img"][view] = cropped_imgs
+            filled_prompt.append(obj_repr)
+    raw_prompt = [filled_prompt]
+    max_n_objs_prompt = {view: 0 for view in views}
+    for prompt in raw_prompt:
+        for token in prompt:
+            if isinstance(token, dict):
+                for view in views:
+                    max_n_objs_prompt[view] = max(
+                        max_n_objs_prompt[view], len(token["cropped_img"][view])
+                    )
+    raw_prompt_token_type, word_batch, image_batch = [], [], []
+    for prompt in raw_prompt:
+        token_type = []
+        for token in prompt:
+            if isinstance(token, int):
+                token_type.append(0)
+                word_batch.append(token)
+            elif isinstance(token, dict):
+                token_type.append(1)
+                n_objs_prompt = {
+                    view: len(token["cropped_img"][view]) for view in views
+                }
+                # add mask
+                token["mask"] = {
+                    view: np.ones((n_objs_prompt[view],), dtype=np.bool_)
+                    for view in views
+                }
+                n_objs_to_pad = {
+                    view: max_n_objs_prompt[view] - n_objs_prompt[view]
+                    for view in views
+                }
+                objs_pad = {
+                    "bbox": {
+                        view: np.zeros((n_objs_to_pad[view], 4), dtype=np.int64)
+                        for view in views
+                    },
+                    "cropped_img": {
+                        view: np.zeros(
+                            (n_objs_to_pad[view], 3, 32, 32),
+                            dtype=np.uint8,
+                        )
+                        for view in views
+                    },
+                    "mask": {
+                        view: np.zeros((n_objs_to_pad[view]), dtype=np.bool_)
+                        for view in views
+                    },
+                }
+                token = any_concat([token, objs_pad], dim=0)
+                image_batch.append(token)
+        raw_prompt_token_type.append(token_type)
+    assert sum([len(prompt) for prompt in raw_prompt_token_type]) == len(
+        word_batch
+    ) + len(image_batch)
+    word_batch = any_stack(word_batch, dim=0)
+    image_batch = any_to_datadict(stack_sequence_fields(image_batch))
+
+    word_batch = any_to_torch_tensor(word_batch)
+    image_batch = image_batch.to_torch_tensor()
+    return raw_prompt_token_type, word_batch, image_batch
